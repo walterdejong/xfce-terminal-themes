f68fb4923a6dbc1b7257927a80b171396bc5f1f8
Walter de Jong <walter@heiho.net>
2024-06-16 15:57:39 +0200

    XFCE Terminal colorschemes

---
+++ install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+DESTDIR="$HOME/.local/share/xfce4/terminal/colorschemes"
+
+if [[ "$1" != "-f" ]]
+then
+	echo "warning: NOTHING DONE"
+	echo "use ${0##*/} -f to really install into $DESTDIR/"
+	exit 1
+fi
+
+if [[ ! -f walter-vga.theme ]]
+then
+	echo "error: files not found"
+	echo "please chdir to source directory"
+	exit 1
+fi
+
+set -x
+
+install -m 0644 -D walter-vga.theme "$DESTDIR/walter-vga.theme"
+install -m 0644 -D walter-amber.theme "$DESTDIR/walter-amber.theme"
+
+# EOB
